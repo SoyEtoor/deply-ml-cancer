ee25a7a054033ea151f7e71883acfdbf84d80bc3
SoyEtoor <torreshectormig@gmail.com>
2024-10-24 17:11:18 -0600

    add breast-model

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pathlib
 from fastapi.middleware.cors import CORSMiddleware
 
 origins = ["*"]
-app = FastAPI(title = 'Heart Disease Prediction')
+app = FastAPI(title = 'Breast Cancer Prediction')
 
 app.add_middleware(
    CORSMiddleware,
@@ -16,22 +16,40 @@ app.add_middleware(
    allow_headers=["*"]
 )
 
-model = load(pathlib.Path('model/heart-disease-v1.joblib'))
+model = load(pathlib.Path('model/breast_cancer_model.joblib'))
 
 class InputData(BaseModel):
-    age:int=64
-    sex:int=1
-    cp:int=3
-    trestbps:int=120
-    chol:int=267
-    fbs:int=0
-    restecg:int=0
-    thalach:int=99
-    exang:int=1
-    oldpeak:float=1.8
-    slope:int=1
-    ca:int=2
-    thal:int=2
+    diagnosis: float = 569.000000
+    radius_mean: float = 17.99
+    texture_mean: float = 10.38
+    perimeter_mean: float = 122.80
+    area_mean: float = 1001.0
+    smoothness_mean: float = 0.11840
+    compactness_mean: float = 0.27760
+    concavity_mean: float = 0.3001
+    concave_points_mean: float = 0.14710
+    symmetry_mean: float = 0.2419
+    fractal_dimension_mean: float = 0.07864
+    radius_se: float = 1.095
+    texture_se: float = 0.9053
+    perimeter_se: float = 8.589
+    area_se: float = 153.4
+    smoothness_se: float = 0.00645
+    compactness_se: float = 0.04904
+    concavity_se: float = 0.05373
+    concave_points_se: float = 0.01587
+    symmetry_se: float = 0.03022
+    fractal_dimension_se: float = 0.00615
+    radius_worst: float = 25.38
+    texture_worst: float = 17.33
+    perimeter_worst: float = 184.60
+    area_worst: float = 2019.0
+    smoothness_worst: float = 0.1622
+    compactness_worst: float = 0.6656
+    concavity_worst: float = 0.7119
+    concave_points_worst: float = 0.2654
+    symmetry_worst: float = 0.4601
+    fractal_dimension_worst: float = 0.11890
 
 class OutputData(BaseModel):
     score:float=0.80318881046519
--- model/train.py
+++ model/train.py
@@ -1,22 +1,63 @@
-from os import PathLike
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-from joblib import dump
-import pandas as pd
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load
+
 import pathlib
+from joblib import dump
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
-df = pd.read_csv(pathlib.Path('data/heart-disease.csv'))
-y = df.pop('target')
+# Input data files are available in the read-only "../input/" directory
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
+import os
+
+from requests import head
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
+# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
+
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import jaccard_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_curve
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score, roc_auc_score
+from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import cross_validate
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import HistGradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+df = pd.read_csv('model/data.csv')
+
+import os
+# print(df.head())
+# print(df.shape)
+# print(df['diagnosis'].value_counts())
+
+# labelencoder = LabelEncoder()
+# df['diagnosis'] = labelencoder.fit_transform(df['diagnosis'].values)
+
+# print(df['diagnosis'].value_counts())
+
+df = pd.read_csv(pathlib.Path('model/data.csv')) 
+y = df.pop('diagnosis') 
 X = df
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2)
 
-print ('Training model.. ')
-clf = RandomForestClassifier(n_estimators = 10,
-                            max_depth=2,
-                            random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print('Entrenando el modelo..')
+clf = RandomForestClassifier(n_estimators=100, random_state=42)
 clf.fit(X_train, y_train)
-print ('Saving model..')
 
-dump(clf, pathlib.Path('model/heart-disease-v1.joblib'))
-
+print('Guardando el modelo..')
+dump(clf, pathlib.Path('model/breast_cancer_model.joblib'))
